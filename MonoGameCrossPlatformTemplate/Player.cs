befef22da8f89954742821e6ad2654d41137bb67
johnbenfer <jbenfer@ksu.edu>
2020-02-25 12:44:47 -0600

    adding player sprites

--- MonoGameCrossPlatformTemplate/Player.cs
+++ MonoGameCrossPlatformTemplate/Player.cs
@@ -8,10 +8,10 @@ namespace CIS580Project4
     public enum PlayerState
     {
         Moving,
-        Jumping,
         Shooting,
         Standing
     }
+
 
     public enum PlayerLocation
     {
@@ -21,35 +21,76 @@ namespace CIS580Project4
 
     public class Player
     {
+        Texture2D standingRight;
+        Texture2D standingLeft;
+        Texture2D[] jumpingRight = new Texture2D[4];
+        Texture2D[] jumpingLeft = new Texture2D[4];
+        Texture2D[] runningRight = new Texture2D[4];
+        Texture2D[] runningLeft = new Texture2D[4];
+
+        bool jumping;
         double playerSpeedX;
         double playerSpeedY;
         PlayerState action;
         PlayerLocation location;
-        
+        double MAX_JUMP_HEIGHT;
+        double MAX_X_SPEED;
+        double MAX_Y_SPEED;
         double playerX;
         double playerY;
+        Game1 game;
 
-        public Player()
+        public Player(Game1 game)
         {
             LoadContent();
 
-
-
+            MAX_JUMP_HEIGHT = 100;
+            MAX_X_SPEED = 10;
+            MAX_Y_SPEED = 15;
+            playerSpeedX = 0;
+            playerSpeedY = 0;
+            jumping = false;
+            this.game = game;
 
         }
 
         public void Update()
 		{
+            var keyboard = Keyboard.GetState();
 
             switch (action)
 			{
                 case PlayerState.Moving:
-					
-                    break;
-                case PlayerState.Jumping:
-                    if(location == PlayerLocation.Air)
+					if(keyboard.IsKeyDown(Keys.Right))
                     {
-
+                        if (playerSpeedX < MAX_X_SPEED)
+                        {
+                            if(playerSpeedX < 0)
+                            {
+                                playerSpeedX += 3;
+                            } else
+                            {
+                                playerSpeedX += 0.6;
+                            }
+                            
+                        }
+                        playerX += playerSpeedX;
+                        game.ViewportX -= playerSpeedX;
+                    }
+                    if (keyboard.IsKeyDown(Keys.Left))
+                    {
+                        if (playerSpeedX > MAX_X_SPEED * -1)
+                        {
+                            if (playerSpeedX > 0)
+                            {
+                                playerSpeedX -= 3;
+                            }
+                            else
+                            {
+                                playerSpeedX -= 0.6;
+                            }
+                        }
+                        playerX += playerSpeedX;
                     }
                     break;
                 case PlayerState.Shooting:
@@ -60,15 +101,23 @@ namespace CIS580Project4
                 
 			} // end of state switch
 
-            var keyboard = Keyboard.GetState();
+            
 
             if(keyboard.IsKeyDown(Keys.Space))
             {
-                action = PlayerState.Jumping;
+                action = PlayerState.Moving;
+            }
+            if(keyboard.IsKeyDown(Keys.Right))
+            {
+                action = PlayerState.Moving;
+            }
+            if (keyboard.IsKeyDown(Keys.Left))
+            {
+                action = PlayerState.Moving;
             }
 
 
-		} // end of update method
+        } // end of update method
 
         public void Draw()
 		{
@@ -77,7 +126,7 @@ namespace CIS580Project4
 
         public void LoadContent()
 		{
-
+            standingLeft = game.Content.Load<Texture2D>("StandinLeft");
 		}
 
     }
